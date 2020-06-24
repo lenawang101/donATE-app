c5b3a597f7e2d589efb1707586abf350dbb5c9fd
Lena Wang <lenawxj@gmail.com>
2020-06-24 10:44:54 -0400

    improved home page

---
+++ donATE app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  donATE app
+//
+//  Created by Lena Wang on 6/22/20.
+//  Copyright © 2020 Lena Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
